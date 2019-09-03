6f2680f2830deabb1f0bce8e43224350af5285c6
Eva P <40271421+EvaForEva@users.noreply.github.com>
2019-09-03 11:33:20 -0700

    Add files via upload
    Program converting inches to meters

---
+++ InchesToMeters.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class InchesToMeters {
+    public static void main(String[] args) {
+        double inches, meters;
+        int counter;
+
+        counter = 0;
+        for(inches = 1; inches <= 144; inches++) {
+            meters = inches / 39.37;
+            System.out.println(inches + " inches are " + meters + " meters");
+
+            counter++;
+            if(counter == 12) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
