673fb0ae708d6fc14b236be55bd53d5125f88b1c
Eva P <40271421+EvaForEva@users.noreply.github.com>
2019-09-18 11:26:15 -0700

    Add files via upload
    new programm calculating minutes

---
+++ conditional-statements/Time15Mins.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Time15Mins {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int startHour = Integer.parseInt(scanner.nextLine());
+        int startMinutes = Integer.parseInt(scanner.nextLine());
+        int timeInMinutes = startHour * 60 + startMinutes;
+        int timePlus15 = timeInMinutes + 15;
+        int finalHour = timePlus15 / 60;
+        int finalMinutes = timePlus15 % 60;
+
+        if (finalHour >=24) {
+            finalHour = finalHour - 24;
+        }
+        if (finalMinutes < 10) {
+            System.out.printf ("%d:0%d",finalHour,finalMinutes);
+        } else {
+            System.out.printf ("%d:%d",finalHour,finalMinutes);
+        }
+    }
+}
