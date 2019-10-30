694d0dd487eca5d4947921d68034729a25aa88df
Eva P <40271421+EvaForEva@users.noreply.github.com>
2019-10-29 23:44:53 -0700

    Update Matrix.java
    tabs fix

--- nested-loops/Matrix.java
+++ nested-loops/Matrix.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Matrix {
 
     public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    int a = Integer.parseInt(scanner.nextLine());
+	Scanner scanner = new Scanner(System.in);
+	int a = Integer.parseInt(scanner.nextLine());
         int b = Integer.parseInt(scanner.nextLine());
         int c = Integer.parseInt(scanner.nextLine());
         int d = Integer.parseInt(scanner.nextLine());
