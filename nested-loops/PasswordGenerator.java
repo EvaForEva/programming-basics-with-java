210cfdae38fce596f2280eed41438e47ccd7a78b
Eva P <40271421+EvaForEva@users.noreply.github.com>
2019-10-29 23:45:52 -0700

    Update PasswordGenerator.java
    tabs fix

--- nested-loops/PasswordGenerator.java
+++ nested-loops/PasswordGenerator.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class PasswordGenerator {
 
     public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    int n = Integer.parseInt(scanner.nextLine());
+	Scanner scanner = new Scanner(System.in);
+	int n = Integer.parseInt(scanner.nextLine());
         int l = Integer.parseInt(scanner.nextLine());
 
         for (int i = 1; i < n; i++) {
